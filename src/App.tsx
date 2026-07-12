f248116e828543b59e5ac78bf5615a1c77a4d046
kaleub <111444302+kaleub@users.noreply.github.com>
2026-07-11 19:43:20 -0600

    Strip default UI, bare canvas with image support

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import { Tldraw } from 'tldraw'
 import 'tldraw/tldraw.css'
 
+// Hide every default UI piece we don't need
+const components = {
+  Toolbar: null,
+  StylePanel: null,
+  PageMenu: null,
+  NavigationPanel: null,
+  ZoomMenu: null,
+  MainMenu: null,
+  ContextMenu: null,
+  ActionsMenu: null,
+  HelpMenu: null,
+  DebugPanel: null,
+  DebugMenu: null,
+  MenuPanel: null,
+  TopPanel: null,
+  SharePanel: null,
+  CursorChatBubble: null,
+  KeyboardShortcutsDialog: null,
+}
+
 function App() {
   return (
     <div style={{ position: 'fixed', inset: 0 }}>
-      <Tldraw />
+      <Tldraw components={components} />
     </div>
   )
 }
